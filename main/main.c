408b6858ae044a4c52db942772b7711da20f8d07
ximen <ximen@ximen.ru>
2020-11-18 19:53:47 +0300

    Added BLE Mesh

--- main/main.c
+++ main/main.c
@@ -1,9 +1,21 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "app_config.h"
-
+#include "app_config_ble_mesh.h"
+#include "esp_ble_mesh_generic_model_api.h"
 #define TAG "MAIN"
+
+static void example_ble_mesh_generic_server_cb(esp_ble_mesh_generic_server_cb_event_t event, esp_ble_mesh_generic_server_cb_param_t *param){
+    ESP_LOGI(TAG, "Callback stub");
+}
 
 void app_main(void){
     ESP_ERROR_CHECK(app_config_init());		    // Initializing and loading configuration
-}
+    esp_err_t err = bluetooth_init();
+    if (err) {
+        ESP_LOGE(TAG, "bluetooth_init failed (err %d)", err);
+        return;
+    }
+    esp_ble_mesh_register_generic_server_callback(example_ble_mesh_generic_server_cb);
+    app_config_ble_mesh_init();
+}s
